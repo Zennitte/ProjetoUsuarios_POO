8d278a4c43286730fbd3200a0e394e1724bbd394
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2021-06-08 15:42:21 -0300

    Classe Usuários concluída

--- Interfaces/IUsuario.cs
+++ Interfaces/IUsuario.cs
@@ -1,8 +1,10 @@
+using ProjetoProdutos_POO.Classes;
+
 namespace ProjetoProdutos_POO.Interfaces
 {
     public interface IUsuario
     {
-      
-         
+        string Cadastrar(Usuario usuario);
+        string Deletar(Usuario usuario);
     }
 }
