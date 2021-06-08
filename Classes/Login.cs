36787674c316cabb921b83263252635739a3cd10
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2021-06-08 16:41:32 -0300

    Classe Login parcialmente concluída

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -1,7 +1,47 @@
+using System;
+
 namespace ProjetoProdutos_POO.Classes
 {
     public class Login
     {
-        
+        private bool Logado;
+        public void FazerLogin()
+        {
+            Console.WriteLine(@"
+Olá! O que deseja fazer?
+1 - Cadastrar usuário
+2 - Fazer o Login 
+3 - Sair");
+            string select = Console.ReadLine();
+
+            switch (select)
+            {
+                case "1":
+                    Console.WriteLine("Digite seu nome: ");
+                    string nomeCadastro = Console.ReadLine();
+                    Console.WriteLine("Digite seu código: ");
+                    string codigoCadastro = Console.ReadLine();
+                    Console.WriteLine("Digite seu email: ");
+                    string emailCadastro = Console.ReadLine();
+                    Console.WriteLine("Digite seu senha: ");
+                    string senhaCadastro = Console.ReadLine();
+                    Usuario usuario = new Usuario(nomeCadastro, codigoCadastro, emailCadastro, senhaCadastro);
+                    Usuario.Cadastrar(usuario);
+                    break;
+                case "2":
+                    
+                    break;
+            }
+        }
+        public string Logar(Usuario usuario)
+        {
+
+            return "Batata";
+        }
+        public string Deslogar(Usuario usuario)
+        {
+
+            return "Batata";
+        }
     }
 }
--- Interfaces/ILogin.cs
+++ Interfaces/ILogin.cs
@@ -1,8 +1,12 @@
+using ProjetoProdutos_POO.Classes;
+
 namespace ProjetoProdutos_POO.Interfaces
 {
     public interface ILogin
     {
-         
+         void Login();
+         string Logar(Usuario usuario);
+         string Deslogar(Usuario usuario);
          
     }
 }
